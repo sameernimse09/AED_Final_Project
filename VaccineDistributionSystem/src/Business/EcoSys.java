c52fc964b1103f0fffc030fc7d9919d2a6b59c5c
Anushka Darade <darade.a@northeastern.edu>
2022-12-05 16:54:03 -0500

    connecting to database

---
+++ VaccineDistributionSystem/src/Business/EcoSys.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business;
+
+/**
+ *
+ * @author anushkadarade
+ */
+public class EcoSys {
+    
+    private static EcoSys business;
+    
+    public static EcoSys getInstance(){
+        if(business==null){
+            business=new EcoSys();  
+        }
+        return business;
+    }
+}
