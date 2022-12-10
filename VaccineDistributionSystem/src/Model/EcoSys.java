2658e597b16fcb77ecee4b06ae081def37a41289
Anushka Darade <darade.a@northeastern.edu>
2022-12-10 18:34:27 -0500

    Added Person Role

--- VaccineDistributionSystem/src/Model/EcoSys.java
+++ VaccineDistributionSystem/src/Model/EcoSys.java
@@ -1,21 +1,62 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package Model;
+
+import Model.Network.Network;
+import Model.Organization.Organization;
+import Model.Role.Role;
+import Model.Role.SystemAdminR;
+import java.util.ArrayList;
 
 /**
  *
  * @author anushkadarade
  */
-public class EcoSys {
+public class EcoSys extends Organization{
     
     private static EcoSys business;
-    
+    private ArrayList<Network> networkList;
     public static EcoSys getInstance(){
         if(business==null){
-            business=new EcoSys();  
+            business=new EcoSys();
         }
         return business;
+    }
+    
+    public Network createAndAddNetwork(){
+        Network network=new Network();
+        networkList.add(network);
+        return network;
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roleList=new ArrayList<Role>();
+        roleList.add(new SystemAdminR());
+        return roleList;
+    }
+    private EcoSys(){
+        super(null);
+        networkList=new ArrayList<Network>();
+    }
+
+    public ArrayList<Network> getNetworkList() {
+        return networkList;
+    }
+
+    public void setNetworkList(ArrayList<Network> networkList) {
+        this.networkList = networkList;
+    }
+    
+    public boolean checkIfUserIsUnique(String userName){
+        if(!this.getUserAccountDirectory().checkIfUsernameIsUnique(userName)){
+            return false;
+        }
+        for(Network network:networkList){
+            
+        }
+        return true;
     }
 }
